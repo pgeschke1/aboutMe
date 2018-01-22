c0843d5fe15196b4275fc0f598df8731c0a2215f
pgeschke1 <pgeschke@students.mchenry.edu>
2018-01-22 00:37:29 -0600

    Initial commit

---
+++ src/com/paulgeschke/Main.java
@@ -0,0 +1,11 @@
+package com.paulgeschke;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("My name is Paul.");
+        System.out.println("I am looking forward to learning Java.");
+        System.out.println("So far it is fairly simple.");
+        System.out.println("The saving path for the files seems clunky to me though.");
+    }
+}
